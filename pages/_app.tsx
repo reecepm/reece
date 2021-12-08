96c8c2de7a8b0b1f2c5ba548ae32d6afc0ab946b
Cascoid <cascoidmc@gmail.com>
2021-12-08 18:54:27 +0000

    Favicon, title, seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,28 @@ import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import React from "react";
 import { AnimatePresence } from "framer-motion";
+import Head from "next/head";
+import { DefaultSeo } from "next-seo";
 
 const App = ({ Component, pageProps, router }: AppProps) => {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.png" type="image/png"/>
+				<title>Reece</title>
+      </Head>
+      <DefaultSeo
+        titleTemplate="%s - Reece Martin"
+        openGraph={{
+          type: "website",
+          locale: "en_GB",
+          url: "https://reece.so",
+          description: "Personal website for Reece Martin, software developer.",
+          site_name: "Reece Martin | reece.so",
+          images: [],
+        }}
+        canonical={"https://reece.so"}
+      />
       <AnimatePresence exitBeforeEnter>
         <Layout>
           <Component {...pageProps} key={router.route} />
